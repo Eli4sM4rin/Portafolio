b268b2e50a6070f2243aa32ac78baa0049a3a4b9
Eli4sM4rin <166948277+Eli4sM4rin@users.noreply.github.com>
2024-09-21 12:38:15 -0600

    Actualización del proyecto

--- sw.js
+++ sw.js
@@ -1,100 +1,105 @@
 ;
-// Nombre y versión del caché
-const CACHE_NAME = 'v1_cache_virtual_folio';
-// Archivos a almacenar en caché
-const urlsToCache = [
-  './',
-  './index.html',
-  './css/style.css',
-  './css/style.min.css',
-  './js/main.js',
-  './img/about.jpg',
-  './img/blog-1.jpg',
-  './img/blog-2.jpg',
-  './img/blog-3.jpg',
-  './img/favicon.ico',
-  './img/portfolio-1.jpg',
-  './img/portfolio-2.jpg',
-  './img/portfolio-3.jpg',
-  './img/portfolio-4.jpg',
-  './img/portfolio-5.jpg',
-  './img/portfolio-6.jpg',
-  './img/profile.jpg',
-  './img/testimonial-1.jpg',
-  './img/testimonial-2.jpg',
-  './img/testimonial-3.jpg',
-  './img/screenshots/screenshot1.png',
-  './lib/easing/easing.js',
-  './lib/easing/easing.min.js',
-  './lib/isotope/isotope.pkgd.js',
-  './lib/isotope/isotope.pkgd.min.js',
-  './lib/lightbox/css/lightbox.css',
-  './lib/lightbox/css/lightbox.min.css',
-  './lib/lightbox/images/close.png',
-  './lib/lightbox/images/loading.gif',
-  './lib/lightbox/images/next.png',
-  './lib/lightbox/images/prev.png',
-  './lib/lightbox/js/lightbox.js',
-  './lib/lightbox/js/lightbox.min.js',
-  './lib/owlcarousel/owl.carousel.js',
-  './lib/owlcarousel/owl.carousel.min.js',
-  './lib/owlcarousel/assets/ajax-loader.gif',
-  './lib/owlcarousel/assets/owl.carousel.css',
-  './lib/owlcarousel/assets/owl.carousel.min.css',
-  './lib/owlcarousel/assets/owl.theme.default.css',
-  './lib/owlcarousel/assets/owl.theme.default.min.css',
-  './lib/owlcarousel/assets/owl.theme.green.css',
-  './lib/owlcarousel/assets/owl.theme.green.min.css',
-  './lib/owlcarousel/assets/owl.video.play.png',
-  './lib/typed/typed.js',
-  './lib/typed/typed.min.js',
-  './lib/waypoints/waypoints.min.js',
-  './mail/contact.js',
-  './mail/contact.php',
-  './mail/jqBootstrapValidation.min.js',
-  './lib/waypoints/links.php'
-];
+//asignar un nombre y versión al cache
+const CACHE_NAME = 'v1_cache_programador',
+  urlsToCache = [
+    './',
+    './index.html',
+    './css/style.css',
+    './css/style.min.css',
+    './js/main.js',
+    './img/about.jpg',
+    './img/blog-1.jpg',
+    './img/blog-2.jpg',
+    './img/blog-3.jpg',
+    './img/favicon.ico',
+    './img/portfolio-1.jpg',
+    './img/portfolio-2.jpg',
+    './img/portfolio-3.jpg',
+    './img/portfolio-4.jpg',
+    './img/portfolio-5.jpg',
+    './img/portfolio-6.jpg',
+    './img/profile.jpg',
+    './img/testimonial-1.jpg',
+    './img/testimonial-2.jpg',
+    './img/testimonial-3.jpg',
+    './img/screenshots/screenshot1.png',
+    
+    './lib/easing/easing.js',
+    './lib/easing/easing.min.js',
+    './lib/isotope/isotope.pkgd.js',
+    './lib/isotope/isotope.pkgd.min.js',
+    './lib/lightbox/css/lightbox.css',
+    './lib/lightbox/css/lightbox.min.css',
+    './lib/lightbox/images/close.png',
+    './lib/lightbox/images/loading.gif',
+    './lib/lightbox/images/next.png',
+    './lib/lightbox/images/prev.png',
+    './lib/lightbox/js/lightbox.js',
+    './lib/lightbox/js/lightbox.min.js',
+    './lib/owlcarousel/owl.carousel.js',
+    './lib/owlcarousel/owl.carousel.min.js',
+    './lib/owlcarousel/assets/ajax-loader.gif',
+    './lib/owlcarousel/assets/owl.carousel.css',
+    './lib/owlcarousel/assets/owl.carousel.min.css',
+    './lib/owlcarousel/assets/owl.theme.default.css',
+    './lib/owlcarousel/assets/owl.theme.default.min.css',
+    './lib/owlcarousel/assets/owl.theme.green.css',
+    './lib/owlcarousel/assets/owl.theme.green.min.css',
+    './lib/owlcarousel/assets/owl.video.play.png',
+    './lib/typed/typed.js',
+    './lib/typed/typed.min.js',
+    './lib/waypoints/waypoints.min.js',
+    './mail/contact.js',
+    './mail/contact.php',
+    './mail/jqBootstrapValidation.min.js',
+    './lib/waypoints/links.php'
+  ]
 
+//durante la fase de instalación, generalmente se almacena en caché los activos estáticos
 self.addEventListener('install', e => {
-  console.log('Service Worker installing...');
   e.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Caching files');
-        return cache.addAll(urlsToCache);
+        return cache.addAll(urlsToCache)
+          .then(() => self.skipWaiting())
       })
-      .then(() => {
-        console.log('Skip waiting');
-        self.skipWaiting();
-      })
-      .catch(err => console.error('Falló el almacenamiento en caché:', err))
-  );
-});
+      .catch(err => console.log('Falló registro de cache', err))
+  )
+})
 
+//una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
 self.addEventListener('activate', e => {
-  console.log('Service Worker activating...');
-  const cacheWhitelist = [CACHE_NAME];
+  const cacheWhitelist = [CACHE_NAME]
 
   e.waitUntil(
     caches.keys()
       .then(cacheNames => {
         return Promise.all(
           cacheNames.map(cacheName => {
-            if (!cacheWhitelist.includes(cacheName)) {
-              console.log('Deleting old cache:', cacheName);
-              return caches.delete(cacheName);
+            //Eliminamos lo que ya no se necesita en cache
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName)
             }
           })
-        );
+        )
       })
+      // Le indica al SW activar el cache actual
       .then(() => self.clients.claim())
-  );
-});
+  )
+})
 
+//cuando el navegador recupera una url
 self.addEventListener('fetch', e => {
-  console.log('Fetching:', e.request.url);
+  //Responder ya sea con el objeto en caché o continuar y buscar la url real
   e.respondWith(
     caches.match(e.request)
-      .then(response => response || fetch(e.request))
-  );
-});
+      .then(res => {
+        if (res) {
+          //recuperar del cache
+          return res
+        }
+        //recuperar de la petición a la url
+        return fetch(e.request)
+      })
+  )
+})
